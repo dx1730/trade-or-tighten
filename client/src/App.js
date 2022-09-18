06ffd81fb607af328b28470d48cdd3d5fe5d5a0d
David Xue <davidxue@davids-mbp.lan>
2022-09-17 19:53:43 -0700

    added routing and Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import Join from "./components/Join";
+import Home from "./components/Home";
 import Chat from "./components/Chat";
 import Rules from "./components/Rules";
 import io from "socket.io-client";
@@ -20,24 +20,12 @@ function App() {
     <Router>
       <Nav/>
       <Routes>
-        <Route path="/" element={<Join socket={socket} room={room} setRoom={setRoom} />}/>
+        <Route path="/" element={<Home socket={socket} room={room} setRoom={setRoom} />}/>
         <Route path="/rules" element={<Rules/>} />
         <Route path="/chat" element={<Chat socket={socket} room={room} />} />
       </Routes>
     </Router>
   )
-
-  // return (
-  //   <div className='bg-indigo-100 min-h-screen'>
-  //     <Nav />
-  //     {!joined ? (
-  //       <Join socket={socket} setJoined={setJoined} room={room} setRoom={setRoom} />
-  //     ) : (
-  //       <Chat socket={socket} room={room} />
-  //     )}
-  //     <footer></footer>
-  //   </div>
-  // );
 }
 
 export default App;
